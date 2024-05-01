81a07a81fe1754b09a83dfbaf870635b795c5e5a
rukirukiruk <163348244+rukirukiruk@users.noreply.github.com>
2024-05-01 20:24:16 +0200

    optimization enhancement

--- scheduleTweets.js
+++ scheduleTweets.js
@@ -1,9 +1,10 @@
 const cron = require('node-cron');
-const fs = require('fs');
-const dotenv = require('dotenv');
+const fs = require('fs').promises; // Use fs promises for non-blocking I/O
 const { TwitterApi } = require('twitter-api-v2');
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const twitterClient = new TwitterApi({
     appKey: process.env.TWITTER_API_KEY,
@@ -12,23 +13,22 @@ const twitterClient = new TwitterApi({
     accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-const readTweetsToSchedule = () => {
+const readTweetsToSchedule = async () => {
     try {
-        const data = fs.readFileSync('./scheduled_tweets.json', 'utf8');
+        const data = await fs.readFile('./scheduled_tweets.json', 'utf8');
         return JSON.parse(data);
     } catch (error) {
         console.error('Error reading scheduled tweets file. Ensure the file exists and it is in correct JSON format:', error.message);
-        return []; // Returning an empty array if there is an issue so the rest of the application can continue to run smoothly.
+        return [];
     }
 };
 
-const scheduleTweets = () => {
-    const tweets = readTweetsToSchedule();
+const scheduleTweets = async () => {
+    const tweets = await readTweetsToSchedule();
     
     tweets.forEach(tweet => {
         const { time, content } = tweet;
 
-        // Verify the cron time and tweet content before scheduling
         if (!cron.validate(time)) {
             console.error(`Invalid cron time format for tweet: "${content}". Please review it.`);
             return;
@@ -57,7 +57,6 @@ const scheduleTweets = () => {
     });
 };
 
-// Initial call to schedule tweets
 scheduleTweets();
 
 module.exports = {
