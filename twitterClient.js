6c27ffb5399919c676c4d2e71b9a8171693497ef
rukirukiruk <163348244+rukirukiruk@users.noreply.github.com>
2024-05-01 23:45:28 +0200

    optimization update

--- twitterClient.js
+++ twitterClient.js
@@ -8,14 +8,34 @@ const twitterClient = new Twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
+let tweetsCache = new Map();
+
 function sendTweet(tweetText) {
+  if (tweetsCache.has(tweetText) && (Date.now() - tweetsCache.get(tweetText)) < 3600000) {
+    console.log('Tweet was already sent recently:', tweetText);
+    return;
+  }
+
   twitterClient.post('statuses/update', { status: tweetText }, (err, data, response) => {
     if (err) {
       console.error('Error sending tweet:', err);
     } else {
       console.log('Successfully sent tweet:', data.text);
+      tweetsCache.set(tweetText, Date.now());
     }
   });
 }
+
+function clearOldCacheEntries() {
+  const expiry = 3600000;
+  const now = Date.now();
+  tweetsCache.forEach((value, key) => {
+    if (now - value > expiry) {
+      tweetsCache.delete(key);
+    }
+  });
+}
+
+setInterval(clearOldCacheEntries, 3600000);
 
 module.exports = { twitterClient, sendTweet };
