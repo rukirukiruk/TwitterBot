0caf756c501475f8b48be487b28057ff55eb5fcc
rukirukiruk <163348244+rukirukiruk@users.noreply.github.com>
2024-05-05 01:19:18 +0200

    improve code readability

--- twitterClient.js
+++ twitterClient.js
@@ -2,34 +2,37 @@ require('dotenv').config();
 const Twit = require('twit');
 
 const twitterClient = new Twit({
-  consumer_key: process.env.API_KEY,
-  consumer_secret: process.env.API_SECRET_KEY,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key:         process.env.API_KEY,
+  consumer_secret:      process.env.API_SECRET_KEY,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
 });
 
 let tweetsCache = new Map();
 
 function sendTweet(tweetText) {
-  const tweetCooldownPeriod = 3600000; 
-  if (tweetsCache.has(tweetText) && (Date.now() - tweetsCache.get(tweetText)) < tweetCooldownPeriod) {
+  const tweetCooldownPeriod = 3600000; // 1 hour
+  
+  if (tweetsCache.has(tweetText) && (Date.now() - tweetsCache.get(tweetText) < tweetCooldownPeriod)) {
     console.log('Tweet was already sent recently:', tweetText);
     return;
   }
 
-  twitterClient.post('statuses/update', { status: tweetText }, (err, data) => {
+  twitterClient.post('statuses/update', { status: tweetText }, function(err, data, response) {
     if (err) {
       console.error('Error sending tweet:', err);
     } else {
       console.log('Successfully sent tweet:', data.text);
+      // Store the tweet text with the current timestamp
       tweetsCache.set(tweetText, Date.now());
     }
   });
 }
 
 function clearOldCacheEntries() {
-  const expiry = 3600000;
+  const expiry = 3600000; // 1 hour
   const now = Date.now();
+  
   tweetsCache.forEach((timestamp, tweet) => {
     if (now - timestamp > expiry) {
       tweetsCache.delete(tweet);
@@ -37,6 +40,7 @@ function clearOldCacheEntries() {
   });
 }
 
+// Setup a routine to clear old cache entries every hour
 setInterval(clearOldCacheEntries, 3600000);
 
 module.exports = { twitterClient, sendTweet };
