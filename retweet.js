4a0e84a0d6af2007752d2061ebc036cb3fcbde26
rukirukiruk <163348244+rukirukiruk@users.noreply.github.com>
2024-05-07 05:09:59 +0200

    update error catching

--- retweet.js
+++ retweet.js
@@ -7,7 +7,7 @@ const twitterClient = new Twit({
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000,
+  timeout_ms: 60 * 1000,  // 60 seconds
   strictSSL: true,
 });
 
@@ -15,30 +15,39 @@ const retweetConfig = {
   searchHashtags: ['#exampleHashtag1', '#exampleHashtag2'],
   searchResultType: 'recent',
   searchCount: 10,
-  retweetInterval: 60000,
+  retweetInterval: 60000,  // 1 minute
 };
 
-const retweetBasedOnHashtags = () => {
+const retweetBasedOnHashtags = async () => {
   const searchTerm = retweetConfig.searchHashtags.join(' OR ');
-  
-  twitterClient.get('search/tweets', { q: searchTerm, count: retweetConfig.searchCount, result_type: retweetConfig.searchResultType }, function(err, data) {
-    if (!err) {
-      const foundTweets = data.statuses;
-      
-      foundTweets.forEach(tweet => {
-        twitterClient.post('statuses/retweet/:id', { id: tweet.id_str }, (retweetErr, response) => {
-          if (response) {
-            console.log('Retweeted:', `https://twitter.com/${response.user.screen_name}/status/${response.id_str}`);
-          }
-          if (retweetErr) {
-            console.error('Retweet Failed:', retweetErr);
-          }
-        });
-      });
-    } else {
-      console.error('Search Failed:', err);
+
+  try {
+    const { data } = await twitterClient.get('search/tweets', {
+      q: searchTerm,
+      count: retweetConfig.searchCount,
+      result_type: retweetConfig.searchResultType
+    });
+
+    const foundTweets = data.statuses;
+
+    if (foundTweets.length === 0) {
+      console.log("No tweets found for the hashtags specified.");
+      return;
     }
-  });
+
+    foundTweets.forEach(tweet => {
+      twitterClient.post('statuses/retweet/:id', { id: tweet.id_str }, (retweetErr, response) => {
+        if (response) {
+          console.log('Retweeted:', `https://twitter.com/${response.user.screen_name}/status/${response.id_str}`);
+        }
+        if (retweetErr) {
+          console.error('Retweet Failed:', retweetErr);
+        }
+      });
+    });
+  } catch (err) {
+    console.error('Failed to search tweets or process them:', err);
+  }
 };
 
 const initializeRetweetProcess = () => {
